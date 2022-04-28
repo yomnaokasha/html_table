04f1b86ae37f288a73b49cd4ca414cca9b17df33
yomnaokasha <youmna.okasha@gmail.com>
2022-04-28 14:31:52 -0700

    Initial commit

---
+++ table.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def create_table():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+
+    return render_template('table.html', users_info=users)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5004)
